72724061c406b394ef718134ec3dff1cfb0dda58
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-01-30 11:06:59 -0800

    great progress! author map is now complete.

--- src/licenser.sh
+++ src/licenser.sh
@@ -52,28 +52,40 @@ done
 
 split_text=" LICENSER_SPLIT "
 
-getLog() {
+get_log() {
   git log --author-date-order --reverse --date=short --pretty=format:"%an${split_text}%as" $1
 }
 
-getYear() {
+get_year() {
   echo "$1" | grep --only-matching -E '\b[0-9]{4}'
 }
 
-isInArray() {
-  inputArray=$1
-  inputItem=$2
-  for i in $inputArray; do
-  done
+simple_array_contains() { # tests if an array contains a given value, but cannot handle spaces, etc.
+  pattern=$1              # usage: [[ $(simple_array_contains $pattern_to_check ${array_name[@]})
+  shift
+  [[ "$@" =~ $pattern ]] && echo "true" || echo "false"
 }
 
-log=$(getLog $1)
+log=$(get_log $1)
 
 authors=($(echo "$log" | awk -F "$split_text" '{print $1}'))
 dates=($(echo "$log" | awk -F "$split_text" '{print $2}'))
 both=($(echo "$log" | awk -F "$split_text" '{print $1, $2}'))
 
-unqiue_authors=()
+declare -A map
 for ((i = 0 ; i < ${#authors[@]} ; i++)); do
-  echo "$i, ${authors[i]}, $(getYear ${dates[i]})"
+  current_author=${authors[$i]}
+  current_year=$(get_year ${dates[$i]})
+  if [[ $(simple_array_contains $current_year ${map[$current_author]}) == false ]]; then
+    map[$current_author]="${map[$current_author]} $current_year"
+  fi
 done
+
+for i in ${!map[@]}; do # for every person in the map...
+  echo "LIST: $i: ${map[$i]}" # list their name and contributed years
+done
+
+#for ((i = 0 ; i < ${#authors[@]} ; i++)); do
+#  echo "$i, ${authors[i]}, $(getYear ${dates[i]})"
+#done
+
