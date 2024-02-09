7e6aea92420b5519b9729319bd46fa0b4b2b840f
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-02-08 18:34:42 -0800

    added a help message

--- src/net/remasteredarch/licenser/Licenser.java
+++ src/net/remasteredarch/licenser/Licenser.java
@@ -1,10 +1,24 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Copyright © 2024 RemasteredArch
+ *
+ * Licenser is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Licenser is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Licenser. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package net.remasteredarch.licenser;
 
-import java.util.*;
-
 public class Licenser {
-	static String reset_text = "\033[0m";
-	static String bold_text = "\033[1m";
+	private final static String version = "v0.1";
+
+	private final static String reset = "\033[0m";
+	private final static String bold = "\033[1m";
+	private final static String italic = "\033[3m";
 
 	public static void main(String[] args) {
 		parseOptions(args);
@@ -12,13 +26,32 @@ public class Licenser {
 
 	private static void parseOptions(String[] args) {
 		for (String arg : args) {
-			if (arg == "--help" || arg == "-h") {
+			if (arg.equals("--help") || arg.equals("-h")) {
 				helpMessage();
 			}
 		}
 	}
 
 	private static void helpMessage() {
-		System.out.println(bold_text + "Licenser:" + reset_text);
+		System.out.println(bold + "Licenser " + italic + version + ":" + reset
+				+ " Recursively apply a license and copyright header to an existing codebase for GPL compliance");
+		System.out.println(bold + "\nUsage: " + reset + "java Licenser.java " + italic + "[path] [options]" + reset);
+		System.out.println(bold + "\nPath:" + reset);
+		System.out.println("  Provide a directory to recursively apply to all files within the given directory");
+		System.out.println("  Provide a file to only apply to the given file");
+		System.out.println("  Provide no path to use the current working directory");
+		System.out.println(bold + "\nOptions:" + reset);
+		System.out.println("  -h | --help     " + italic + "Displays this help message" + reset);
+		System.out.println("  -v | --version  " + italic + "Displays the version of the program" + reset);
+		System.out.println(
+				"  -d | --dry-run  " + italic + "Displays the effects of a given input without actually running." + reset);
+		System.out.println(italic + "\n\nLicenser is work in progress software. " + bold + "Use at your own risk!" + reset);
+		System.out.println(italic
+				+ "\n\nLicenser is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.");
+		System.out.println(
+				"\nLicenser is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.");
+		System.out.println(
+				"\nYou should have received a copy of the GNU General Public License along with Licenser. If not, see <https://www.gnu.org/licenses/>."
+						+ reset);
 	}
 }
